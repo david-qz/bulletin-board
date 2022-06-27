af69092730fab37130b28b09b5e7dd9c27fd930f
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-27 14:29:34 -0700

    Start implementing services

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 // import services and utilities
+import { getUser, signOut } from '/services/auth-service.js';
+import { getPosts } from '/services/data-service.js';
 
 // import component creators
 
 // declare state variables
+let posts = [];
 
 // write handler functions
+async function handlePageLoad() {
+    posts = await getPosts();
 
-// Create each component: 
+    display();
+}
+
+// Create each component:
 // - pass in the root element via querySelector
-// - pass any needed handler functions as properties of an actions object 
+// - pass any needed handler functions as properties of an actions object
 
 // Roll-up display function that renders (calls with state) each component
 function display() {
@@ -16,4 +24,4 @@ function display() {
 }
 
 // Call display on page load
-display();
+handlePageLoad();
