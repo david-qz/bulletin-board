acc056c05023f09ee86e88f7c7eb95bfacb40b96
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-27 15:06:36 -0700

    Implement sign in/out toggle component

--- app.js
+++ app.js
@@ -3,27 +3,48 @@ import { getUser, signOut } from '/services/auth-service.js';
 import { getBulletins } from '/services/data-service.js';
 
 // import component creators
+import createSignInOut from './components/SignInOut.js';
 import createBulletinList from '/components/BulletinList.js';
 
-// declare state variables
-let bulletins = [];
+// declare state
+const state = {
+    user: null,
+    bulletins: [],
+    get signedIn() {
+        return !!this.user;
+    }
+};
 
 // write handler functions
 async function handlePageLoad() {
-    bulletins = await getBulletins();
+    state.bulletins = await getBulletins();
+    state.user = await getUser();
 
     display();
 }
 
+async function handleSignInOut() {
+    if (state.signedIn) {
+        await signOut();
+    } else {
+        location = '/auth';
+    }
+}
+
 // Create each component:
-// - pass in the root element via querySelector
-// - pass any needed handler functions as properties of an actions object
+const SignInOut = createSignInOut(document.querySelector('#sign-in-out'), {
+    handleSignInOut
+});
 const BulletinList = createBulletinList(document.querySelector('#bulletin-list'));
 
 // Roll-up display function that renders (calls with state) each component
 function display() {
-    BulletinList({ bulletins });
+    SignInOut({
+        signedIn: state.signedIn,
+        username: state.user?.email,
+    });
+    BulletinList({ bulletins: state.bulletins });
 }
 
-// Call display on page load
+// Do page load actions
 handlePageLoad();
