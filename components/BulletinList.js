8206db78cd7e1cfeef81ca223156e5dc5659d487
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-27 17:27:31 -0700

    Style bulletin list

--- components/BulletinList.js
+++ components/BulletinList.js
@@ -12,15 +12,19 @@ export default function createBulletinList(root) {
 function createBulletin(bulletin) {
     const titleElement = document.createElement('h3');
     titleElement.textContent = bulletin.title;
+    titleElement.classList.add('title');
 
     const contentElement = document.createElement('p');
     contentElement.textContent = bulletin.content;
+    contentElement.classList.add('content');
 
     const posterElement = document.createElement('p');
     posterElement.textContent = bulletin.userContact;
+    posterElement.classList.add('contact');
 
     const timestampElement = document.createElement('p');
     timestampElement.textContent = bulletin.createdAt;
+    timestampElement.classList.add('timestamp');
 
     const li = document.createElement('li');
     li.append(titleElement, contentElement, posterElement, timestampElement);
