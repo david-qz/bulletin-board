238e2d252cc7a2603ff32ccd4d0e068b4cd863f3
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-27 19:53:43 -0700

    Make the post button visible when signed out
    No sense having the smart redirect feature on the auth
    page if the user can't see it.

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ const BulletinList = createBulletinList(document.querySelector('#bulletin-list')
 
 // Roll-up display function that renders (calls with state) each component
 function display() {
-    NewBulletinButton({ signedIn: state.signedIn });
+    NewBulletinButton({ signedIn: true }); // Leaving this until I decide if this button should hide
     SignInOut({
         signedIn: state.signedIn,
         username: state.user?.email,
