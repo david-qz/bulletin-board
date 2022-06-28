aac7695c5d7c70c9330f60185de3a94c002b2d02
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-27 17:47:27 -0700

    Parse timestamp for display

--- components/BulletinList.js
+++ components/BulletinList.js
@@ -23,7 +23,7 @@ function createBulletin(bulletin) {
     userInfoElement.classList.add('contact');
 
     const timestampElement = document.createElement('p');
-    timestampElement.textContent = bulletin.createdAt;
+    timestampElement.textContent = (new Date(bulletin.createdAt)).toLocaleString();
     timestampElement.classList.add('timestamp');
 
     const li = document.createElement('li');
